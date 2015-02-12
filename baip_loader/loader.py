f8de46c0cbd84ea8f89db92bbe9707c19bb2347f
Lou Markovski <lou.markovski@bom.gov.au>
2015-02-12 01:28:39 +0000

    task/ta-26-save-csiro-baip-metadata-to-local-filesystem
    baip_loader/loader.py
    * alter the source() method to solely handle to connection and
      retrieval of the CSIRO data.  Data is stored in the
      new csiro_source_data attribute
    * new method dump_source() that can output the source to a nominated
      output file

--- baip_loader/loader.py
+++ baip_loader/loader.py
@@ -1,20 +1,23 @@
-# pylint: disable=R0903,C0111,R0902
-"""The :class:`baip_loader.Loader` provides file ingest support.
-
-"""
-__all__ = ["Loader"]
-
 import urllib2
+import tempfile
 
 from logga.log import log
+    
+
+__all__ = ["Loader"]
 
 
 class Loader(object):
     """:class:`baip_loader.Loader`
 
-    _csiro_source_uri = None
+    .. attribute:: *csiro_source_uri*
+
+    .. attribute:: *csiro_source_data*
 
     """
+    _csiro_source_uri = None
+    _csiro_source_data = None
+
     def __init__(self, source_uri=None):
         """
         """
@@ -26,20 +29,63 @@ class Loader(object):
         return self._csiro_source_uri
 
     @csiro_source_uri.setter
-    def set_csiro_source_uri(self, value):
+    def csiro_source_uri(self, value):
         self._csiro_source_uri = value
+
+    @property
+    def csiro_source_data(self):
+        return self._csiro_source_data
+
+    @csiro_source_data.setter
+    def csiro_source_data(self, value):
+        self._csiro_source_data = value
 
     def source(self):
         """Attempt to source CSIRO metadata.
+
+        .. note::
+
+            The entire metadata set is stored in memory and can be
+            accessed via the :attr:`csiro_source_data` attribute
 
         """
         msg = 'Sourcing data from {uri} ...'
         log.debug(msg.format(uri=self.csiro_source_uri))
 
         url_obj = urllib2.urlopen(self.csiro_source_uri)
-        content = url_obj.read()
+        self.csiro_source_data = url_obj.read()
 
-        import tempfile
-        file_obj = tempfile.NamedTemporaryFile(delete=False)
-        file_obj.write(content)
-        file_obj.close()
+    def dump_source(self, filename=None):
+        """Write out the contents of the :attr:`csiro_source_data`
+        to *filename* if not `None` or a temporary file using
+        :mod:`tempfile.NamedTemporaryFile`.
+
+        if :attr:`csiro_source_data` is `None` then no attempt to write
+        will be made.
+
+        **Args:**
+            *filename*: full path of the target file to write to
+
+        **Returns:**
+            On write success, the name of the output filename.  ``None``
+            otherwise
+
+        """
+        file_obj = None
+        target_file = None
+
+        if self.csiro_source_data is not None:
+            if filename is None:
+                file_obj = tempfile.NamedTemporaryFile(delete=False)
+            else:
+                file_obj = open(filename, 'w')
+
+            target_file = file_obj.name
+
+            log.info('Writing CSIRO content to {0}'.format(target_file))
+            file_obj.write(self.csiro_source_data)
+            file_obj.close()
+        else:
+            log.info('Source data not defined -- skipping write')
+
+        return target_file
--- baip_loader/tests/test_loader.py
+++ baip_loader/tests/test_loader.py
@@ -1,9 +1,4 @@
-# pylint: disable=R0904,C0103,W0142,W0212
-""":class:`baip_loader.TestLoader` tests.
-
-"""
 import unittest2
-import urlparse
 
 import baip_loader
 
@@ -21,13 +16,3 @@ class TestLoader(unittest2.TestCase):
         loader = baip_loader.Loader()
         msg = 'Object is not a baip_loader.Loader'
         self.assertIsInstance(loader, baip_loader.Loader, msg)
-
-    def test_source(self):
-        """Source CSIRO data.
-        """
-        scheme = 'http'
-        netloc = 'data.bioregionalassessments.gov.au'
-        path = '/function/metadataexport/dd006fce-bef5-4377-82ae-2c5a14b50e34'
-        source_uri = urlparse.urlunsplit((scheme, netloc, path, None, None))
-        loader = baip_loader.Loader(source_uri)
-        loader.source()
