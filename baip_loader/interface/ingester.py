c47f51de5e6df45a856c04cb9d5e10da488d7cbb
Lou Markovski <lou.markovski@gmail.com>
2015-03-19 05:27:33 +0000

    task/ta-154-ingester-interface-ingest-json-to-ckan
    baip_loader/interface/ingester.py
    * new method ingest() that handles the CKAN dataset ingest

--- baip_loader/interface/ingester.py
+++ baip_loader/interface/ingester.py
@@ -1,6 +1,8 @@
 import os
+import json
+import urllib
+import urllib2
 
-import baip_loader
 from logga.log import log
 from filer.files import get_directory_files_list
 
@@ -15,11 +17,12 @@ class Ingester(object):
 
     """
     _source_dir = None
+    _api_key = None
+    _csiro_uri = None
 
     def __init__(self, source_dir=None):
-        """
-        """
-        self._source_dir = source_dir
+        if source_dir is not None:
+            self._source_dir = source_dir
 
     @property
     def source_dir(self):
@@ -28,6 +31,22 @@ class Ingester(object):
     @source_dir.setter
     def source_dir(self, value):
         self._source_dir = value
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, value):
+        self._api_key = value
+
+    @property
+    def csiro_uri(self):
+        return self._csiro_uri
+
+    @csiro_uri.setter
+    def csiro_uri(self, value):
+        self._csiro_uri = value
 
     def source_files(self, file_filter=None):
         """Checks :attr:`source_dir` for valid CKAN JSON files.
@@ -55,3 +74,39 @@ class Ingester(object):
         log.debug('CKAN JSON files sourced: %s' % files_to_process)
 
         return files_to_process
+
+    def ingest(self, ckan_json, dry=False):
+        """Ingest dataset into CKAN.
+
+        **Args:**
+            *ckan_json*: the CKAN JSON ingest structure
+
+            *dry*: only report, do not execute
+
+        **Returns:**
+
+            boolean ``True`` on success, ``False`` otherwise
+
+        """
+        log.info('Attempting CKAN ingest ...')
+
+        data_string = urllib.quote(ckan_json)
+
+        log.debug('Preparing request to "%s"' % self.csiro_uri)
+        request = urllib2.Request(self.csiro_uri)
+        request.add_header('Authorization', self.api_key)
+
+        status = True
+        if not dry:
+            try:
+                response = urllib2.urlopen(request, data_string)
+                assert(response.code == 200)
+
+                response_dict = json.loads(response.read())
+                assert(response_dict['success'] is True)
+            except urllib2.HTTPError as err:
+                log.error('Error code|msg: {0}|{1}'.format(err.msg,
+                                                           err.headers))
+                status = False
+
+        return status
--- baip_loader/interface/tests/test_ingester.py
+++ baip_loader/interface/tests/test_ingester.py
@@ -13,7 +13,12 @@ class TestIngester(unittest2.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.maxDiff = None
-        cls._source_dir = os.path.join('baip_loader', 'tests', 'files')
+        test_file_dir = os.path.join('baip_loader',
+                                     'interface',
+                                     'tests',
+                                     'files')
+        test_json_file = 'DD006FCE-BEF5-4377-82AE-2C5A14B50E34.json'
+        cls._test_json_path = os.path.join(test_file_dir, test_json_file)
 
     def test_init(self):
         """Initialise a baip_loader.Ingester object.
@@ -51,8 +56,8 @@ class TestIngester(unittest2.TestCase):
         source_files = ['457ED79A-C6DF-4AAF-A480-00926E48CAA8.json',
                         '6151C409-0CAF-4727-9F57-00F6B71A58FB.json',
                         'banana']
-        for file in source_files:
-            source_fh = open(os.path.join(source_dir, file), 'w')
+        for source_file in source_files:
+            source_fh = open(os.path.join(source_dir, source_file), 'w')
             source_fh.close()
 
         # when I want to source JSON files
@@ -78,8 +83,8 @@ class TestIngester(unittest2.TestCase):
         source_files = ['457ED79A-C6DF-4AAF-A480-00926E48CAA8.json',
                         '6151C409-0CAF-4727-9F57-00F6B71A58FB.json']
         invalid_source_files = ['apples', 'banana']
-        for file in (source_files + invalid_source_files):
-            source_fh = open(os.path.join(source_dir, file), 'w')
+        for source_file in (source_files + invalid_source_files):
+            source_fh = open(os.path.join(source_dir, source_file), 'w')
             source_fh.close()
 
         # when I want to source JSON files
@@ -94,6 +99,29 @@ class TestIngester(unittest2.TestCase):
         remove_files(get_directory_files_list(source_dir))
         os.removedirs(source_dir)
 
+    def test_ingest(self):
+        """CKAN JSON dataset ingest.
+        """
+        # Given a CKAN JSON file
+        ckan_fh = open(self._test_json_path)
+        ckan_json = ckan_fh.read()
+        ckan_fh.close()
+
+        # and a CKAN API configuration has been set
+        ingester = baip_loader.Ingester()
+        ingester.api_key = '524130fc-da5e-4d8c-b5c6-3e980b02f148'
+        scheme = 'http'
+        netloc = 'test.ddg.lws.links.com.au'
+        path = '/api/action/package_create'
+        ingester.csiro_uri = '{0}://{1}{2}'.format(scheme, netloc, path)
+
+        # when I attempt a CKAN dataset ingest
+        received = ingester.ingest(ckan_json, dry=True)
+
+        # then the ingest should succeed
+        msg = 'Ingest should return True'
+        self.assertTrue(received, msg)
+
     @classmethod
     def tearDownClass(cls):
-        cls._source_dir = None
+        cls._test_json_path = None
